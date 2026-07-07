0764c2ddddb2b5c090ba581318d363c29cc653d6
Frank <goodman.capu@gmail.com>
2026-07-06 23:43:45 -0700

    initial widget

---
+++ CAPUBBS Widget/CAPUBBS_WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  CAPUBBS_WidgetBundle.swift
+//  CAPUBBS Widget
+//
+//  Created by Zhikang Fan on 7/6/26.
+//  Copyright © 2026 熊典. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CAPUBBS_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CAPUBBS_Widget()
+    }
+}
